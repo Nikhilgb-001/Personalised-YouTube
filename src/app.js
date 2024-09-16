836efe5c57912f57d546066deb44bac52c007f01
Nikhil <nikhilgb3113@gmail.com>
2024-09-16 13:28:33 +0530

    added register user controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -13,14 +13,19 @@ app.use(
 
 // accepting front form data
 app.use(express.json({ limit: "16kb" }));
-
 // accepting url data and encoding them
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 // serving static files
 app.use(express.static("public"));
-
 // accepting cookies and manipulate them
 app.use(cookieParser());
+
+// routes import
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+// url creates as http://localhost:8000/api/v1/users/register
 
 export { app };
